4f75d8b3420c3397257a0822e81258206f3456b1
Alessandro Dias <alebatista90@terra.com.br>
2014-04-11 02:07:25 -0300

    using postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140410060256) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: true do |t|
     t.string  "task"
     t.integer "status", default: 0
